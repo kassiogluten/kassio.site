4fa1f0ecb409138e010d5ec784f7b8da39e98556
Kassio <kassiogluten@gmail.com>
2021-07-12 23:32:28 -0300

    best layout in the world bahia

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,35 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Kássio Alcantara</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        <img width="100px" src="https://github.com/kassiogluten.png" alt="Kássio Alcantara"></img>
+        <h1>Kássio Alcantara</h1>
+        <h3>Programador Front-End</h3>
+        <h5>Freelancer desenvolvendo aplicações e páginas com NextJS e React Native</h5>
+        <ul>
+          <li>
+            <a href="https://www.fb.com/kassiogluten">Facebook</a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/kassiogluten/">Instagram</a>
+          </li>
+          <li>
+            <a href="https://api.whatsapp.com/send?phone=5533920001670">Whatsapp</a>
+          </li>
+          <li>
+            <a href="https://t.me/kassiogluten">Telegram</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/kassio-alcantara-gluten-ab2b75149/">Linkedin</a>
+          </li>
+          <li>
+            <a href="https://github.com/kassiogluten">Github</a>
+          </li>
+        </ul>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
